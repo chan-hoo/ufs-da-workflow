accae5b73056e5f129f5b985b679af0384eee698
chan-hoo <chan-hoo.jeon@noaa.gov>
2025-09-11 11:57:49 -0500

    update module files

--- modulefiles/ufsda_gaeac6.intel.lua
+++ modulefiles/ufsda_gaeac6.intel.lua
@@ -1,9 +1,8 @@
 help([[
-  This module loads libraries required for building and running UFS Weather Model 
-  on the NOAA RDHPC machine Gaea C6 using Intel-2023.2.0.
+  This module loads libraries required for building UFS-DA-Workflow
+  on the NOAA RDHPCS machine Gaea C6 using Intel
 ]])
-
-whatis([===[Loads libraries needed for building the UFS Weather Model on Gaea C6]===])
+whatis([===[Loads libraries needed for building the UFS-DA-Workflow on Gaea C6]===])
 
 prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/spack-stack-1.9.2/envs/ue-intel-2023.2.0/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/modulefiles")
@@ -20,11 +19,25 @@ load(pathJoin("stack-python", stack_python_ver))
 cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 load(pathJoin("cmake", cmake_ver))
 
+ecbuild_ver=os.getenv("ecbuild_ver") or "3.7.2"
+load(pathJoin("ecbuild", ecbuild_ver))
+
 load("ufs_common")
-load("zlib/1.2.13")
 
 nccmp_ver=os.getenv("nccmp_ver") or "1.9.0.1"
 load(pathJoin("nccmp", nccmp_ver))
+
+nco_ver=os.getenv("nco_ver") or "5.2.4"
+load(pathJoin("nco", nco_ver))
+
+nemsio_ver=os.getenv("nemsio_ver") or "2.5.4"
+load(pathJoin("nemsio", nemsio_ver))
+
+sigio_ver=os.getenv("sigio_ver") or "2.3.3"
+load(pathJoin("sigio", sigio_ver))
+
+zlib_ver=os.getenv("zlib_ver") or "1.2.13"
+load(pathJoin("zlib", zlib_ver))
 
 unload("darshan-runtime")
 unload("cray-libsci")
--- modulefiles/ufsda_hera.intel.lua
+++ modulefiles/ufsda_hera.intel.lua
@@ -1,6 +1,8 @@
 help([[
-loads UFS Model prerequisites for Hera/Intel
+  This module loads libraries required for building UFS-DA-Workflow
+  on the NOAA RDHPCS machine Hera using Intel
 ]])
+whatis([===[Loads libraries needed for building the UFS-DA-Workflow on Hera]===])
 
 prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/intel-oneapi-mpi/2021.13-sbi3u54/gcc/13.3.0")
@@ -14,11 +16,25 @@ load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 load(pathJoin("cmake", cmake_ver))
 
+ecbuild_ver=os.getenv("ecbuild_ver") or "3.7.2"
+load(pathJoin("ecbuild", ecbuild_ver))
+
 load("ufs_common")
-load("zlib/1.2.11")
 
 nccmp_ver=os.getenv("nccmp_ver") or "1.9.0.1"
 load(pathJoin("nccmp", nccmp_ver))
+
+nco_ver=os.getenv("nco_ver") or "5.2.4"
+load(pathJoin("nco", nco_ver))
+
+nemsio_ver=os.getenv("nemsio_ver") or "2.5.4"
+load(pathJoin("nemsio", nemsio_ver))
+
+sigio_ver=os.getenv("sigio_ver") or "2.3.3"
+load(pathJoin("sigio", sigio_ver))
+
+zlib_ver=os.getenv("zlib_ver") or "1.2.13"
+load(pathJoin("zlib", zlib_ver))
 
 setenv("CC", "mpiicx")
 setenv("CXX", "mpiicpx")
