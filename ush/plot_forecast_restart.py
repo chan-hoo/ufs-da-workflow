4b599c7aa797b19572e95a461bdf90a0582aea46
chan-hoo <chan-hoo.jeon@noaa.gov>
2025-09-19 13:46:22 -0500

    add smc to plot restart script

--- ush/plot_forecast_restart.py
+++ ush/plot_forecast_restart.py
@@ -58,7 +58,7 @@ def main():
         log_level_str = "INFO"
         log_level = logging.INFO
         print(f''' WARNING: Invalid log level "{PY_LOG_LEVEL.upper()}", set to INFO.''')
-    print(f''' Python Log Level= str: {log_level_str}, attr: {log_level}''')
+    print(f''' Python Log Level = str: {log_level_str}, attr: {log_level}''')
     logging.basicConfig(format='%(levelname)s::%(pathname)s::L%(lineno)d::%(message)s', level=log_level)
 
     logging.info(f''' YAML Data: {yaml_data}''')
@@ -69,8 +69,7 @@ def main():
     # get lon, lat from orography
     get_geo(orog_path,orog_fn_base)
 
-#    var_list=["snwdph","smc"]
-    var_list=["snwdph"]
+    var_list=["snwdph","smc"]
 
     # plot restart file
     for var_nm in var_list:
@@ -83,19 +82,19 @@ def get_geo(orog_path,orog_fn_base):
     global glon,glat
     logging.info(f''' ===== geo data files ==============================================''')
 
-    glon_all=[]
-    glat_all=[]
+    glon_all = []
+    glat_all = []
     for it in range(num_tiles):
-        itp=it+1
-        fn_orog=f'''{orog_fn_base}.tile{itp}.nc'''
-        fp_orog=os.path.join(orog_path,fn_orog)
+        itp = it+1
+        fn_orog = f'''{orog_fn_base}.tile{itp}.nc'''
+        fp_orog = os.path.join(orog_path,fn_orog)
 
-        try: orog=xr.open_dataset(fp_orog)
+        try: orog = xr.open_dataset(fp_orog)
         except: raise Exception('Could NOT find the file',fp_orog)
 
         # Extract longitudes, and latitudes
-        geolon=np.ma.masked_invalid(orog['geolon'].data)
-        geolat=np.ma.masked_invalid(orog['geolat'].data)
+        geolon = np.ma.masked_invalid(orog['geolon'].data)
+        geolat = np.ma.masked_invalid(orog['geolat'].data)
 
         logging.info(f''' Dimension of glon (tile {itp}) = {geolon.shape}''')
         logging.info(f''' Tile{itp}, Max = {np.max(geolon)}''')
@@ -107,74 +106,84 @@ def get_geo(orog_path,orog_fn_base):
         glon_all.append(geolon[None,:])
         glat_all.append(geolat[None,:])
 
-        if itp==1:
-            print(orog)
+        if itp == 1:
+            logging.info(f''' Variables: {list(orog.variables)}''')
 
-    glon=np.vstack(glon_all)
-    glat=np.vstack(glat_all)
+    glon = np.vstack(glon_all)
+    glat = np.vstack(glat_all)
 
-    logging.info(f''' Dimension of glon= {glon.shape}''')
-    logging.info(f''' Dimension of glon= {glat.shape}''')
+    logging.info(f''' Dimension of glon = {glon.shape}''')
+    logging.info(f''' Dimension of glon = {glat.shape}''')
 
 
 # Get sfc_data from files and plot ================================== CHJ =====
 def plot_data(path_data,fn_data_base,var_nm,zlvlm1,out_title_base,out_fn_base,work_dir):
 
     # center of map
-    c_lon=-77.0369
+    c_lon = -77.0369
+
+    zlvl = zlvlm1+1
 
     logging.info(f''' ===== data file: '{var_nm}' ========================''')
     # open the data file
     for it in range(num_tiles):
-        itp=it+1
-        fn_data=fn_data_base+str(itp)+'.nc'
-        fp_data=os.path.join(path_data,fn_data)
-        try: data_raw=nc.Dataset(fp_data)
+        itp = it+1
+        fn_data = fn_data_base+str(itp)+'.nc'
+        fp_data = os.path.join(path_data,fn_data)
+        try: data_raw = nc.Dataset(fp_data)
         except: raise Exception('Could NOT find the file',fp_data)
+        if itp == 1:
+            logging.info(f''' Variables: {list(data_raw.variables)}''')
+
         # Extract valid variable
-        var_data=np.ma.masked_invalid(data_raw.variables[var_nm])
-        if var_nm == 'stc' or var_nm == 'smc' or var_nm == 'slc':
-            logging.info(f''' Dimension of original data= {var_data.shape}''')
-            var_data_2d=var_data[:,zlvlm1,:,:]
+        var_data = np.ma.masked_invalid(data_raw.variables[var_nm])
+        ndim_var = var_data.ndim
+        logging.info(f''' {var_nm}: number of dimensions = {ndim_var}''')
+        if ndim_var == 4:
+            logging.info(f''' Dimension of original data = {var_data.shape}, z-level = {zlvl}''')
+            var_data_2d = var_data[:,zlvlm1,:,:]
         else:
-            var_data_2d=var_data                
+            var_data_2d = var_data                
  
-        logging.info(f''' Dimension of data= {var_data_2d.shape}''')
-        logging.info(f''' Tile{itp}, Max= {np.max(var_data_2d)}''')
-        logging.info(f''' Tile{itp}, Min= {np.min(var_data_2d)}''')
+        logging.info(f''' Dimension of data = {var_data_2d.shape}''')
+        logging.info(f''' Tile{itp}, Max = {np.max(var_data_2d)}''')
+        logging.info(f''' Tile{itp}, Min = {np.min(var_data_2d)}''')
 
         if itp == 1:
-            plt_var=var_data_2d
+            plt_var = var_data_2d
         else:
-            plt_var=np.ma.concatenate((plt_var,var_data_2d),axis=0)
+            plt_var = np.ma.concatenate((plt_var,var_data_2d),axis=0)
         data_raw.close()
 
-#    plt_var=np.vstack(var_data_all)
-    logging.info(f''' Dimension of data set= {plt_var.shape}''')
+    logging.info(f''' Dimension of data set = {plt_var.shape}''')
 
-    cs_max=np.max(plt_var)
-    cs_min=np.min(plt_var)
-    logging.info(f''' cs_max= {cs_max}''')
-    logging.info(f''' cs_min= {cs_min}''')
+    cs_max = np.nanmax(plt_var)
+    cs_min = np.nanmin(plt_var)
+    logging.info(f''' cs_max = {cs_max}''')
+    logging.info(f''' cs_min = {cs_min}''')
 
-    cs_cmap='gist_ncar_r'
-    cbar_extend='neither'
+    cs_cmap = 'gist_ncar_r'
+    cbar_extend = 'neither'
 
     # Plot each tile
     for it in range(num_tiles):
-        itp=it+1
-        glon_tile=np.squeeze(glon[it,:,:])
+        itp = it+1
+        glon_tile = np.squeeze(glon[it,:,:])
         if itp == 1:
-            glon_tile=(glon_tile+180)%360-180
-        glat_tile=np.squeeze(glat[it,:,:])
-        var_tile=np.squeeze(plt_var[it,:,:])
-        c_glon=np.round(np.mean(glon_tile),decimals=2)
-        c_glat=np.round(np.mean(glat_tile),decimals=2)
-        logging.info(f'''c_glon, c_glat for tile{str(it+1)}= {c_glon}, {c_glat}''')
-        out_title=out_title_base+var_nm+'::Tile'+str(itp)
-        out_fn=out_fn_base+var_nm+'_tile'+str(itp)
+            glon_tile = (glon_tile+180)%360-180
+        glat_tile = np.squeeze(glat[it,:,:])
+        var_tile = np.squeeze(plt_var[it,:,:])
+        c_glon = np.round(np.mean(glon_tile),decimals=2)
+        c_glat = np.round(np.mean(glat_tile),decimals=2)
+        logging.info(f'''c_glon, c_glat for tile{str(it+1)} = {c_glon}, {c_glat}''')
+        if ndim_var == 4:
+            out_title = f'''{out_title_base}{var_nm}::L{zlvl}+::Tile{itp}'''
+            out_fn = f'''{out_fn_base}{var_nm}_z{zlvl}_tile{itp}'''
+        else:
+            out_title = f'''{out_title_base}{var_nm}::Tile{itp}'''
+            out_fn = f'''{out_fn_base}{var_nm}_tile{itp}'''
 
-        fig,ax=plt.subplots(1,1,subplot_kw=dict(projection=ccrs.Orthographic(c_glon,c_glat)))
+        fig,ax = plt.subplots(1,1,subplot_kw=dict(projection=ccrs.Orthographic(c_glon,c_glat)))
         ax.set_title(out_title, fontsize=6)
         # Call background plot
         back_plot(ax)
@@ -191,8 +200,12 @@ def plot_data(path_data,fn_data_base,var_nm,zlvlm1,out_title_base,out_fn_base,wo
         out_file(work_dir,out_fn,ndpi)
 
     # Plot all tiles together
-    out_title=f'''{out_title_base}{var_nm}::All tiles'''
-    out_fn=f'''{out_fn_base}{var_nm}_alltiles'''
+    if ndim_var == 4:
+        out_title = f'''{out_title_base}{var_nm}::L{zlvl}::All tiles'''
+        out_fn = f'''{out_fn_base}{var_nm}_z{zlvl}_alltiles'''
+    else:
+        out_title = f'''{out_title_base}{var_nm}::All tiles'''
+        out_fn = f'''{out_fn_base}{var_nm}_alltiles'''
 
     fig,ax=plt.subplots(1,1,subplot_kw=dict(projection=ccrs.Robinson(c_lon)))
     ax.set_title(out_title, fontsize=6)
@@ -214,7 +227,7 @@ def plot_data(path_data,fn_data_base,var_nm,zlvlm1,out_title_base,out_fn_base,wo
     cbar.ax.tick_params(labelsize=6)
     cbar.set_label(var_nm,fontsize=6)
     # Output figure
-    ndpi=300
+    ndpi = 300
     out_file(work_dir,out_fn,ndpi)
 
 
@@ -223,25 +236,25 @@ def back_plot(ax):
 # ==================================================================== CHJ =====
 
     # Resolution of background natural earth data ('50m' or '110m')
-    back_res='50m'
+    back_res = '50m'
 
-    fline_wd=0.5  # line width
-    falpha=0.7 # transparency
+    fline_wd = 0.5  # line width
+    falpha = 0.7 # transparency
 
     # natural_earth
-    land=cfeature.NaturalEarthFeature('physical','land',back_res,
+    land = cfeature.NaturalEarthFeature('physical','land',back_res,
                       edgecolor='face',facecolor=cfeature.COLORS['land'],
                       alpha=falpha)
-    lakes=cfeature.NaturalEarthFeature('physical','lakes',back_res,
+    lakes = cfeature.NaturalEarthFeature('physical','lakes',back_res,
                       edgecolor='blue',facecolor='none',
                       linewidth=fline_wd,alpha=falpha)
-    coastline=cfeature.NaturalEarthFeature('physical','coastline',
+    coastline = cfeature.NaturalEarthFeature('physical','coastline',
                       back_res,edgecolor='black',facecolor='none',
                       linewidth=fline_wd,alpha=falpha)
-    states=cfeature.NaturalEarthFeature('cultural','admin_1_states_provinces',
+    states = cfeature.NaturalEarthFeature('cultural','admin_1_states_provinces',
                       back_res,edgecolor='green',facecolor='none',
                       linewidth=fline_wd,linestyle=':',alpha=falpha)
-    borders=cfeature.NaturalEarthFeature('cultural','admin_0_countries',
+    borders = cfeature.NaturalEarthFeature('cultural','admin_0_countries',
                       back_res,edgecolor='red',facecolor='none',
                       linewidth=fline_wd,alpha=falpha)
 
@@ -256,7 +269,7 @@ def back_plot(ax):
 def out_file(work_dir,out_file,ndpi):
 # =================================================================== CHJ =====
     # Output figure
-    fp_out=os.path.join(work_dir,out_file)
+    fp_out = os.path.join(work_dir,out_file)
     plt.savefig(fp_out+'.png',dpi=ndpi,bbox_inches='tight')
     plt.close('all')
 
