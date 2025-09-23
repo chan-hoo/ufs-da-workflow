f2a5a64f9dbbda13030b7cdabda03226202c76f4
chan-hoo <chan-hoo.jeon@noaa.gov>
2025-09-23 18:34:52 +0000

    add envir to avoid structure conflict

--- parm/setup_wflow_env.py
+++ parm/setup_wflow_env.py
@@ -280,16 +280,17 @@ def setup_wflow_env(machine):
     os.chmod(fp_auto_script_expt, 0o755)
 
     # Add links to log/tmp/com directories within exp_case directory
-    model_ver = config_parm.get("model_ver")    
+    envir = config_parm.get("envir")
+    model_ver = config_parm.get("model_ver")
     net = config_parm.get("NET")
     run = config_parm.get("RUN")
     # This path (ptmp) need to be changed only for NOAA NCO EE2 compliance (ptmp=OPSROOT)
-    ptmp = os.path.join(exp_basedir,"ptmp")
-    log_dir_src = os.path.join(ptmp, "com/output/logs")
+    ptmp = os.path.join(exp_basedir, "ptmp")
+    log_dir_src = os.path.join(ptmp, envir, "com/output/logs")
     log_dir_dst = os.path.join(exp_case_path, "log_dir")
-    tmp_dir_src = os.path.join(ptmp, "tmp")
+    tmp_dir_src = os.path.join(ptmp, envir, "tmp")
     tmp_dir_dst = os.path.join(exp_case_path, "tmp_dir")
-    com_dir_src = os.path.join(ptmp, "com", net, model_ver)
+    com_dir_src = os.path.join(ptmp, envir, "com", net, model_ver)
     com_dir_dst = os.path.join(exp_case_path, "com_dir")
     os.symlink(log_dir_src, log_dir_dst)
     os.symlink(tmp_dir_src, tmp_dir_dst)
@@ -331,6 +332,7 @@ def set_default_parm():
         "DT_ATMOS": 720,
         "DT_RUNSEQ": 3600,
         "EXP_CASE_NAME": None,
+        "envir": "test",
         "FCST_HRS": 24,
         "FHROT": 0,
         "FRAC_GRID": "NO",
