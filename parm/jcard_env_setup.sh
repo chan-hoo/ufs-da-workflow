89023167c0b632a6fbb96ef029a27b70cd335f39
chan-hoo <chan-hoo.jeon@noaa.gov>
2025-11-23 17:39:47 -0600

    add more nco variables

--- parm/jcard_env_setup.sh
+++ parm/jcard_env_setup.sh
@@ -9,6 +9,14 @@
 #
 export COMROOT="${COMROOT:-${PTMP}/${envir}/com}"
 export DATAROOT="${DATAROOT:-${PTMP}/${envir}/tmp}"
+export DCOMROOT="${DCOMROOT:-${PTMP}/${envir}/dcom}"
+export KEEPDATA="${KEEPDATA:-YES}"
+export MAILCC="${MAILCC:-None}"
+export MAILTO="${MAILTO:-None}"
+export SENDCOM="${SENDCOM:-NO}"
+export SENDDBN="${SENDDBN:-NO}"
+export SENDECF="${SENDECF:-NO}"
+export SENDWEB="${SENDWEB:-NO}"
 
 export LOGDIR="${LOGDIR:-${COMROOT}/output/logs}"
 
