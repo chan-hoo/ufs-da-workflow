f675528656375eb808d46c0858a62bfac412b7ff
Chan-Hoo Jeon <chan-hoo.jeon@noaa.gov>
2025-12-14 13:46:55 -0600

    add ecflow module for orion

---
+++ modulefiles/wflow_ecflow_orion.lua
@@ -0,0 +1,13 @@
+help([[
+This loads the modules and py environment for running the UFS-DA workflow
+with Rocoto on the MSU machine Orion
+]])
+
+prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneapi-2024.1.0/install/modulefiles/Core")
+load("stack-oneapi/2024.2.1")
+load("stack-intel-oneapi-mpi/2021.13")
+load("ecflow/5.11.4")
+
+prepend_path("MODULEPATH","/work/noaa/epic/UFS-conda/modulefiles")
+load("python-ufs-land-da-wflow")
+
