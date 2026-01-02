dce865f1678e5230c08cd1b624780cf55346e307
chan-hoo <chan-hoo.jeon@noaa.gov>
2026-01-02 23:37:56 +0000

    merge two ecf def templates

--- parm/setup_wflow_env.py
+++ parm/setup_wflow_env.py
@@ -770,8 +770,6 @@ def create_ecflow_files(home_dir,config_parm):
     logging.debug(f''' Data for ecFlow def file: {data_set_str}''')
     if do_free_forecast == "all" and date_cycle_freq_hr < 24:
         fn_ecf_template = "template.ufsda.def_1d2c"
-    elif coldstart == "YES":
-        fn_ecf_template = "template.ufsda.def_coldstart"
     else:
         fn_ecf_template = "template.ufsda.def"
     fp_ecf_template = os.path.join(home_dir,"ecf/defs",fn_ecf_template)
