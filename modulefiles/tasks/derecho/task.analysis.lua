7cdf46edb52f4be85639b17bd377f8a19486d912
chan-hoo <chan-hoo.jeon@noaa.gov>
2026-01-16 15:41:37 -0700

    fix task module files for derecho

--- modulefiles/tasks/derecho/task.analysis.lua
+++ modulefiles/tasks/derecho/task.analysis.lua
@@ -2,6 +2,7 @@ prepend_path("MODULEPATH", os.getenv("modulepath_spack_stack"))
 load(pathJoin("stack-oneapi", stack_intel_ver))
 load(pathJoin("prod_util", prod_util_ver))
 
+load("mkl/2024.2.2")
 load("ecflow/5.11.4")
 
 prepend_path("MODULEPATH", os.getenv("modulepath_pymodule"))
--- modulefiles/tasks/derecho/task.forecast.lua
+++ modulefiles/tasks/derecho/task.forecast.lua
@@ -28,4 +28,5 @@ load(pathJoin("py-netcdf4", py_netcdf4_ver))
 load(pathJoin("py-numpy", py_numpy_ver))
 load(pathJoin("py-pyyaml", py_pyyaml_ver))
 
+load("mkl/2024.2.2")
 load("ecflow/5.11.4")
