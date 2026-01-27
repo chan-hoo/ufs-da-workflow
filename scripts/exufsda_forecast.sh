4046ad09e1259fde7acb128a560a4cff41c49e74
Chan-Hoo.Jeon-NOAA <60152248+chan-hoo@users.noreply.github.com>
2026-01-27 13:28:08 -0500

    Add ATML option of UFS weather model (#20)

--- scripts/exufsda_forecast.sh
+++ scripts/exufsda_forecast.sh
@@ -542,7 +542,7 @@ fi
 # fd_ufs.yaml
 cp -p "${PARMufsda}/templates/template.fd_ufs.yaml" fd_ufs.yaml
 # data_table
-if [ "${APP}" != "ATM" ]; then
+if [ "${APP}" != "ATM" ] && [ "${APP}" != "ATML" ]; then
   cp -p "${PARMufsda}/templates/template.data_table" data_table
 fi
 
@@ -714,6 +714,7 @@ if [ "${lnd_model}" = "noahmp" ]; then
     cp -p "${DATA}/ufs.cpld.lnd.ini.${YYYY}-${MM}-${DD}-${HHsec_5d}.tile${itile}.nc" "${COMINOUT}/${NET}.${cycle}.lnd.f000.c${RES}.tile${itile}.nc"
   done
   ## ufs.cpld.lnd.out
+  icnt="0"
   list_out_fh_lnd=$(seq ${OUTPUT_FH_LND} ${OUTPUT_FH_LND} ${FCST_HRS})
   for ihr in ${list_out_fh_lnd}
   do
