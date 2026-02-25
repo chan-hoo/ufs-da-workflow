928027e433f8e1fc34a4d4b2133c62df8881d9e5
chan-hoo <chan-hoo.jeon@noaa.gov>
2026-02-25 08:29:50 -0700

    fix pbs native cmd

--- parm/setup_wflow_env.py
+++ parm/setup_wflow_env.py
@@ -939,7 +939,7 @@ def set_machine_parm(machine):
     match lowercase_machine:
         case "derecho":
             MAX_CORES_PER_NODE = 128
-            NATIVE = 'place=excl'
+            NATIVE = '-l place=excl'
             PARTITION_QUEUE = "main"
             QOS = None
             RUN_CMD = "aprun"
